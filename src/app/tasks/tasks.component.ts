99e2170eaf5489e8fdff6b3529d9f8078942715b
preet <preet@PREETHS_PC>
2024-09-09 12:18:01 -0400

    first commit

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { UserComponent } from '../user/user.component';
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [UserComponent, TaskComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+
+export class TasksComponent {
+  @Input({required:true}) userId!:string;
+  @Input({required:true}) name!:string;
+  isAddingTask = false;
+
+  constructor(private tasksService: TasksService){}
+
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
+  }
+
+  onCompleteTask(id: string){
+    
+  }
+
+  onStartAddTask() {
+    this.isAddingTask=true;
+
+  }
+
+  onCloseAddTask(){
+    this.isAddingTask=false;
+  }
+
+}
