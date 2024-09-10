5dd33f73ce124478d0ee45171dd47b30155c0cfb
preet <preet@PREETHS_PC>
2024-09-10 11:31:32 -0400

    Coverted Standalone project in module based project

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { UserComponent } from '../user/user.component';
-import { TaskComponent } from "./task/task.component";
-import { NewTaskComponent } from "./new-task/new-task.component";
-import { type NewTaskData } from './task/task.model';
+import { Component, Input} from '@angular/core';
+
 import { TasksService } from './tasks.service';
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [UserComponent, TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,25 +1,20 @@
-import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, NgModule, Output} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { type User } from './user.model';
-import { CardComponent } from "../shared/card/card.component";
 const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
 
 
 @Component({
   selector: 'app-user',
-  standalone: true,
-  imports: [CardComponent],
+  standalone: false,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
 export class UserComponent {
 
   @Input() user!: User
   @Input({required:true}) selected! : boolean;
-  // @Input({required:true}) id!:string;
-
-  // @Input({required:true}) avatar!: string;//avatar =input.requied<string>();
-  // @Input({required:true}) name!:string;//name =input.requied<string>();
   @Output() select = new EventEmitter<string>();
   get imagePath(){
     return 'assets/users/' + this.user.avatar;
